880dbce87f97e32aafa519f07f7e2582ce576f0c
isaelmazi7 <isaelmazi7@gmail.com>
2026-05-08 13:26:20 +0200

    Add files via upload

--- task.cpp
+++ task.cpp
@@ -7,5 +7,7 @@ cout << "This is my first C++ file." << endl;
 
   cout << "I am learning C++ and GitHub." << endl;
 cout << "This is my second commit." << endl;
+cout << "I downloaded this project and edited it on my computer." << endl;
+cout << "Now I will upload it again to GitHub." << endl;
 return 0;
 }
